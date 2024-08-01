3cf63bc76e4f97d158827e42c5ec4ede5bd6a5a5
TimusVladimir <timusvladimir@gmail.com>
2024-08-01 15:18:04 +0300

    проект рабочий, включена защита от парсинга, необхоимо перейти на selenium

--- avito_parser/main.py
+++ avito_parser/main.py
@@ -3,6 +3,7 @@
 from avito_parser.scraper import fetch_page
 from avito_parser.parser import parse_listings
 from avito_parser.storage import save_to_csv
+from avito_parser.utils import save_html
 
 
 def main() -> None:
@@ -10,11 +11,14 @@ def main() -> None:
     Основная функция для запуска парсера Avito.
     """
     # URL страницы с объявлениями (пример)
-    # url = "https://www.avito.ru/moskva/avtomobili"
-    url = "https://www.avito.ru/moskva/avtomobili?radius=0&searchRadius=0"
+    url = "https://www.avito.ru/moskva/avtomobili"
 
     # Получаем HTML-контент страницы
     page_html = fetch_page(url)
+
+    # Сохраняем HTML-контент в файл
+    save_html(page_html, "page.html")
+    print("HTML-страница сохранена в 'page.html'.")
 
     # Парсим данные объявлений
     listings = parse_listings(page_html)
--- avito_parser/utils.py
+++ avito_parser/utils.py
@@ -1,3 +1,11 @@
 # avito_parser/utils.py
 
-# В текущей версии файл может быть пустым или содержать вспомогательные функции
+def save_html(content: str, filename: str) -> None:
+    """
+    Сохраняет HTML-контент в файл.
+
+    :param content: HTML-контент страницы.
+    :param filename: Имя файла для сохранения.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
